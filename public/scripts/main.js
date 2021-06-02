c774314c79426cf4539cbfa2c766d9e0144cbf16
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-06-02 13:17:12 +0200

    Navigation bar and menu

---
+++ public/scripts/main.js
@@ -0,0 +1,31 @@
+const burgerIcon = document.querySelector(".navigation-bar__burger-icon");
+const burgerIconComponents = document.querySelectorAll(".navigation-bar__burger-icon div");
+const navigationMenu = document.querySelector(".navigation-menu");
+const menuElements = document.querySelectorAll(".navigation-menu__main-link, .navigation-menu__line-break, .navigation-menu__social-links");
+
+burgerIcon.addEventListener("click", () => {
+    burgerIcon.classList.toggle("unclickable");
+    navigationMenu.classList.toggle("active");
+    console.log(burgerIconComponents);
+    burgerIconComponents.forEach(component => {
+        component.classList.toggle("active");
+    });
+    setTimeout(async() => {
+        for (let element of menuElements) {
+            await fadeInLink(element);
+        };
+    }, 250);
+    setTimeout(() => {
+        burgerIcon.classList.toggle("unclickable");
+    }, 1000);
+});
+
+const fadeInLink = link => {
+    const promise = new Promise((res, rej) => {
+        setTimeout(() => {
+            link.classList.toggle("fadeFromBottom");
+            res();
+        }, 100);
+    });
+    return promise;
+};

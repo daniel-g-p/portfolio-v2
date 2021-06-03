d368deea9dcf06f9c5e4e67d2f5e95af19f72092
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-06-03 08:40:15 +0200

    Hero and about section

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,21 +3,21 @@ const burgerIconComponents = document.querySelectorAll(".navigation-bar__burger-
 const navigationMenu = document.querySelector(".navigation-menu");
 const menuElements = document.querySelectorAll(".navigation-menu__main-link, .navigation-menu__line-break, .navigation-menu__social-links");
 
-burgerIcon.addEventListener("click", () => {
-    burgerIcon.classList.toggle("unclickable");
+burgerIcon.addEventListener("click", async() => {
+    // burgerIcon.classList.toggle("unclickable");
     navigationMenu.classList.toggle("active");
     console.log(burgerIconComponents);
     burgerIconComponents.forEach(component => {
         component.classList.toggle("active");
     });
-    setTimeout(async() => {
-        for (let element of menuElements) {
-            await fadeInLink(element);
-        };
-    }, 250);
-    setTimeout(() => {
-        burgerIcon.classList.toggle("unclickable");
-    }, 1000);
+    // setTimeout(async() => {
+    for (let element of menuElements) {
+        await fadeInLink(element);
+    };
+    // }, 250);
+    // setTimeout(() => {
+    //     burgerIcon.classList.toggle("unclickable");
+    // }, 1000);
 });
 
 const fadeInLink = link => {
